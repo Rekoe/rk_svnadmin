3b014956d66d72a3bb7854f284a6d02c82e0ecaf
rekoe <koukou890@qq.com>
2016-03-21 18:29:23 +0800

    fix

--- src/main/resources/init_system_h2.sql
+++ src/main/resources/init_system_h2.sql
@@ -5,15 +5,12 @@ INSERT INTO system_user(ID,NAME,PASSWORD,SALT,DESCRIPTION,IS_LOCKED,CREATE_DATE,
 INSERT INTO system_role(ID,NAME,DESCRIPTION) VALUES
 (1,'admin','超级管理员：拥有全部权限的角色');
 /*  .system_permission   		*/
-INSERT INTO `system_permission` VALUES (1,'*:*:*','全部权限','1',1),(2,'system.role','对用户分配角色 ','721fbbca1ae44716918fdb3921deacb1',0),(3,'system.user','添加管理员','721fbbca1ae44716918fdb3921deacb1',0),(4,'system.permission','权限管理','721fbbca1ae44716918fdb3921deacb1',0);
+INSERT INTO `system_permission` VALUES (1,'*:*:*','全部权限','1',1);
 /*  .permission_category   		*/
-INSERT INTO `permission_category` VALUES ('1','超级权限',1),('2','临时权限',1),('721fbbca1ae44716918fdb3921deacb1','用户管理',1);
+INSERT INTO `permission_category` VALUES ('1','超级权限',1);
 /*  .system_user_role   		*/
 INSERT INTO system_user_role(USERID,ROLEID) VALUES 
 (1,1);
 /*  .system_role_permission  		*/
 INSERT INTO system_role_permission(ROLEID,PERMISSIONID) VALUES 
-(1,1),
-(1,2),
-(1,3),
-(1,4);
+(1,1)
